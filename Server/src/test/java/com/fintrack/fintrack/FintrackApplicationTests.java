56105c13e4170a6016d410233e53cf59cf08a3eb
tejas <tejasjagdale2003@gmail.com>
2024-06-16 04:31:37 +0530

    setup commit

---
+++ Server/src/test/java/com/fintrack/fintrack/FintrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintrack.fintrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
