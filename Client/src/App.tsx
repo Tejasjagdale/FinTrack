2ce16e106062d7d2f86007e98c5cacd5d2d774c5
tejas <tejasjagdale2003@gmail.com>
2024-06-17 16:10:45 +0530

    added logo

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '/fintrack.jpg'
 import './App.css'
 
 function App() {
@@ -9,24 +8,17 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={viteLogo} className="logo" alt="Vite logo" />
       </div>
-      <h1>Vite + React</h1>
+      <h1>Wellcome to <b style={{ color: "#476EE2" }}>FinTrack</b></h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          A comprehensive app for tracking expenses, managing income, setting budgets,<br />
+          and achieving savings goals with real-time insights and secure, user-friendly features.
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Comming soon...
       </p>
     </>
   )
