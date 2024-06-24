d4c77ecf0be37e6db1b1816d21a952f36f350c1b
tejas <tejasjagdale2003@gmail.com>
2024-06-24 21:57:54 +0530

    #6 404 issue

--- Client/src/pages/Signup.tsx
+++ Client/src/pages/Signup.tsx
@@ -5,13 +5,13 @@ import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Link } from 'react-router-dom';
 
 
 // TODO remove, this demo shouldn't need to reset the theme.
@@ -106,7 +106,7 @@ export default function SignUp() {
                         </Button>
                         <Grid container justifyContent="flex-end">
                             <Grid item>
-                                <Link href="/login" variant="body2">
+                                <Link to="/login" >
                                     Already have an account? Sign in
                                 </Link>
                             </Grid>
