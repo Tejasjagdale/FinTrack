9ec599480e682ca42cdf7d88b4d7034983434f6d
tejas <tejasjagdale2003@gmail.com>
2024-06-24 21:48:43 +0530

    #6 navigation for login and signup

--- Client/src/pages/Home.tsx
+++ Client/src/pages/Home.tsx
@@ -1,13 +1,17 @@
-import { Box, Button } from '@mui/material'
-import viteLogo from '/beaverimg.png'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Box, Button } from '@mui/material';
+import viteLogo from '/beaverimg.png';
 
 const Home = () => {
+    const navigate = useNavigate();
+
     return (
         <>
             <div>
                 <img src={viteLogo} className="logo" alt="Vite logo" />
             </div>
-            <h1>Wellcome to <b style={{ color: "#476EE2" }}>FinTrack</b></h1>
+            <h1>Welcome to <b style={{ color: "#476EE2" }}>FinTrack</b></h1>
             <div className="card">
                 <p>
                     A comprehensive app for tracking expenses, managing income, setting budgets,<br />
@@ -16,15 +20,15 @@ const Home = () => {
             </div>
             <Box sx={{
                 display: "flex",
-                width: "50%",
-                justifyContent:"space-around",
-                margin:"auto"
+                width: "100%",
+                justifyContent: "space-around",
+                margin: "auto"
             }}>
-                <Button variant="contained">Login</Button>
-                <Button variant="outlined">SignUp</Button>
+                <Button variant="contained" onClick={() => navigate('/login')}>Login</Button>
+                <Button variant="outlined" onClick={() => navigate('/signup')}>SignUp</Button>
             </Box>
         </>
     )
 }
 
-export default Home
+export default Home;
